fd57e65615fab623c089d9a25da82ec186a573a1
gitroman38 <gb5888010@gmail.com>
2022-03-28 22:34:42 +0300

    - added post message to iframe

--- src/vuejs-sdk.vue
+++ src/vuejs-sdk.vue
@@ -1,12 +1,13 @@
 <template>
-  <div class="vue-sdk">
-    <iframe :height="height" :src="url" :width="width" v-bind="$attrs"/>
-  </div>
+  <iframe :id="iframeId" :height="height" :src="url" :width="width" v-bind="$attrs"/>
 </template>
 
 <script>
 export default {
   name: 'vuejs-sdk',
+  data: () => ({
+    iframeId: null
+  }),
   props: {
     width: {
       type: [Number, String],
@@ -20,28 +21,60 @@ export default {
       type: String,
       required: true
     },
-    network: {
-      type: String,
-      validator: value => ['ropsten', 'ethereum'].includes(value)
-    },
-    collectionSlug: String,
-    amount: Number,
-    showTitle: Boolean,
-    amountSelector: Boolean,
-    saleSelector: Boolean,
-    saleId: Number,
-    showDetails: Boolean,
-    showErrors: Boolean,
-    theme: {},
-    styles: {
+    iframeConfig: {
       type: Object,
-      default: () => {}
-    },
-    texts: {
-      type: Object,
-      default: () => {}
-    },
+      default: () => {
+      }
+    }
+    // network: {
+    //   type: String,
+    //   validator: value => ['ropsten', 'ethereum'].includes(value)
+    // },
+    // collectionSlug: String,
+    // amount: Number,
+    // showTitle: Boolean,
+    // amountSelector: Boolean,
+    // saleSelector: Boolean,
+    // saleId: Number,
+    // showDetails: Boolean,
+    // showErrors: Boolean,
+    // theme: {},
+    // styles: {
+    //   type: Object,
+    //   default: () => {
+    //   }
+    // },
+    // texts: {
+    //   type: Object,
+    //   default: () => {
+    //   }
+    // },
   },
+  created() {
+    this.iframeId = this.makeId(10)
+  },
+  beforeMount() {
+    const frame = this.$el || false
+    if (frame) {
+      frame.onload = () => {
+        frame.contentWindow.postMessage(
+            Object.assign({}, this.iframeConfig, {type: 'vuejs-sdk-config'}),
+            '*'
+        );
+      };
+    }
+  },
+  methods: {
+    makeId(length) {
+      let result = '';
+      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      const charactersLength = characters.length;
+      for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+      return result;
+    }
+  }
 };
 </script>
 
